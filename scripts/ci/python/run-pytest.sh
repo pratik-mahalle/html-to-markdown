6717c8ff8667b61a510582720ae87730bec3c6b0
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2025-12-22 08:30:14 +0100

    fix(ci): install python package for tests

--- scripts/ci/python/run-pytest.sh
+++ scripts/ci/python/run-pytest.sh
@@ -4,4 +4,5 @@ set -euo pipefail
 PYTEST_ADDOPTS="${PYTEST_ADDOPTS:--vv --maxfail=1 --durations=25}"
 
 IFS=" " read -r -a pytest_addopts <<<"$PYTEST_ADDOPTS"
+uv pip install --editable packages/python
 uv run pytest "${pytest_addopts[@]}"
