1ebabce013b0bab8915cbb7ff93aaaab994ea822
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2026-01-13 17:36:38 +0100

    fix: Maven Central publishing and version bump to 2.22.1

--- scripts/publish/maven/release-package.sh
+++ scripts/publish/maven/release-package.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-mvn -f packages/java/pom.xml --batch-mode --no-transfer-progress -Dgpg.executable=/usr/bin/gpg2 clean deploy
+mvn -f packages/java/pom.xml -Ppublish -DskipTests --batch-mode --no-transfer-progress -Dgpg.executable=/usr/bin/gpg2 clean deploy
